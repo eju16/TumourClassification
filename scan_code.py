18dd95421143135a15dbf7b0469c552fe0c1d2db
eju16 <88635508+eju16@users.noreply.github.com>
2024-12-09 15:59:01 +0000

    working with smaller files
    split the files to work in smaller batches, since long loading times
    -need to add code to view each slice in a file

--- scan_code.py
+++ scan_code.py
@@ -52,35 +52,44 @@ def get_nifti_file_paths(folder):
                 nifti_files.append(os.path.join(root, file))
     return nifti_files
 
-# Collect paths for normal and abnormal scans
-normal_scan_paths = get_nifti_file_paths("BraTS-Africa/51_OtherNeoplasms")
-abnormal_scan_paths = get_nifti_file_paths("BraTS-Africa/95_Glioma")
+#paths for normal and abnormal scans
+# normal_scan_paths = get_nifti_file_paths("BraTS-Africa/51_OtherNeoplasms")
+# abnormal_scan_paths = get_nifti_file_paths("BraTS-Africa/95_Glioma")
 
-print("Non-cancerous scans: " + str(len(normal_scan_paths)))
-print("Cancerous scans: " + str(len(abnormal_scan_paths)))
+# print("Non-cancerous scans: " + str(len(normal_scan_paths)))
+# print("Cancerous scans: " + str(len(abnormal_scan_paths)))
 
-# Process scans
-normal_scans = np.array([process_scan(path) for path in normal_scan_paths])
-abnormal_scans = np.array([process_scan(path) for path in abnormal_scan_paths])
+# print("checkpoint")
+#-------------------------------------------------------------------
+#process scans
+# normal_scans = np.array([process_scan(path) for path in normal_scan_paths])
+# abnormal_scans = np.array([process_scan(path) for path in abnormal_scan_paths])
 
-print(f"Processed {len(normal_scans)} normal scans.")
-print(f"Processed {len(abnormal_scans)} abnormal scans.")
+# print(f"Processed {len(normal_scans)} normal scans.")
+# print(f"Processed {len(abnormal_scans)} abnormal scans.")
+
+scan = read_nifti_file("BraTS-Africa/95_Glioma/BraTS-SSA-00228-000/BraTS-SSA-00228-000-t2f.nii.gz")
+print(scan.shape)
+#visualise using matplotlib
 
 ###building and training validation datasets
-for path in abnormal_scan_paths:
-    print("in looop")
-    print(path)
+# for path in abnormal_scan_paths:
+#     print("in looop")
+#     print(path)
 
-print("---------1-------")
-#reading and processing the scans
-abnormal_scans = np.array([process_scan(path) for path in abnormal_scan_paths])
-normal_scans = np.array([process_scan(path) for path in normal_scan_paths])
-print("--------2-------")
-#for ct scans having presence of brain tumours: assign 1
-#for normal ones assign 0
-abnormal_labels = np.array([1 for _ in range(len(abnormal_scans))])
-normal_labels = np.array([0 for _ in range(len(normal_scans))])
-print("-------3---------")
+# print("---------1-------")
+# #reading and processing the scans
+# abnormal_scans = np.array([process_scan(path) for path in abnormal_scan_paths])
+# normal_scans = np.array([process_scan(path) for path in normal_scan_paths])
+# print("--------2-------")
+# #for ct scans having presence of brain tumours: assign 1
+# #for normal ones assign 0
+# abnormal_labels = np.array([1 for _ in range(len(abnormal_scans))])
+# normal_labels = np.array([0 for _ in range(len(normal_scans))])
+# print("-------3---------")
+
+
+#the below up to 4 is the main code
 #splitting the data ratio 70-30 for training and validation
 # x_train = np.concatenate((abnormal_scans[:70], normal_scans[:70]), axis=0)
 # y_train = np.concatenate((abnormal_labels[:70], normal_labels[:70]), axis=0)
@@ -88,12 +97,18 @@ print("-------3---------")
 # x_val = np.concatenate((abnormal_scans[70:], normal_scans[70:]), axis=0)
 # y_val = np.concatenate((abnormal_labels[70:], normal_labels[70:]), axis=0)
 print("------4----------")
-#testing with only 51_otherneoplasms
-x_train = np.concatenate((normal_scans[:70]), axis = 0)
-y_train = np.concatenate((normal_labels[:70]), axis = 0)
 
-x_val = np.concatenate((normal_scans[70:]), axis = 0)
-y_val = np.concatenate((normal_labels[70:]), axis=0)
-print("No. samples in train and vaidation are %d and %d" % x_train.shape[0], x_val.shape[0])
-print("--------5--------")
-#need to unzip each .nii.gz folder
+#the below is test code for 3^
+#testing with only 51_otherneoplasms
+
+
+# x_train = np.concatenate((normal_scans[:70]), axis = 0)
+# y_train = np.concatenate((normal_labels[:70]), axis = 0)
+
+# x_val = np.concatenate((normal_scans[70:]), axis = 0)
+# y_val = np.concatenate((normal_labels[70:]), axis=0)
+# print("No. samples in train and vaidation are %d and %d" % x_train.shape[0], x_val.shape[0])
+# print("--------5--------")
+
+#need to unzip each .nii.gz folder (done)
+#split the array/work in smaller batches to avoid long loading times while writin gthe code
